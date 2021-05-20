f69a6fc2b5431a9e0510886ebc0d918a9a0ca8bb
Justin Taylor <justin@labrys.io>
2021-05-20 16:16:20 +1000

    moved constructor

--- contracts/abstract/AbstractLeveragedPool.sol
+++ contracts/abstract/AbstractLeveragedPool.sol
@@ -38,37 +38,15 @@ abstract contract AbstractLeveragedPool {
   uint256 public shadowLongBalance;
   uint256 public shadowShortBalance;
 
-  // #### Functions
-  /**
-  @notice Sets up the contract. Sets the parameters, and creates the two ERC20 tokens for the pool.
-  @param _poolCode The pool's identifier. This will be appended onto a position code (-SHORT, and -LONG) to create the token names.
-  @param _firstPrice The current price for the market
-  @param _updateInterval The minimum amount of time that must elapse before a price update can occur. If the interval is 5 minutes, then the price cannot be updated until 5 minutes after the last update has elapsed.
-  @param _frontRunningInterval The amount of time that must elapse between a commit and the next update interval before a commit can be executed. Must be shorter than the update interval to prevent deadlock.
-  @param _fee The percentage fee that will be charged to the pool's capital on a successful price update
-  @param _leverageAmount The leverage that the pool will expose it's depositors to
-  @param _feeAddress The address that fees will be sent to on every price change
-  @param _quoteToken The address of the digital asset that this pool contains
-   */
-  constructor(
-    string memory _poolCode,
-    uint256 _firstPrice,
-    uint32 _updateInterval,
-    uint32 _frontRunningInterval,
-    uint16 _fee,
-    uint16 _leverageAmount,
-    address _feeAddress,
-    address _quoteToken
-  ) {
-    quoteToken = _quoteToken;
-    lastPrice = _firstPrice;
-    updateInterval = _updateInterval;
-    frontRunningInterval = _frontRunningInterval;
-    fee = _fee;
-    leverageAmount = _leverageAmount;
-    feeAddress = _feeAddress;
-  }
+  // #### Events
+  event TokensCreated(
+    address indexed longToken,
+    address indexed shortToken,
+    uint256 firstPrice,
+    address quoteToken
+  );
 
+  // #### Functions
   /**
     @notice Creates a commitment to mint or burn
     @param commitType Valid types are SB,SM, LB, LM. Each type contains position (Short, Long) and action (Mint, Burn).
--- contracts/implementation/LeveragedPool.sol
+++ contracts/implementation/LeveragedPool.sol
@@ -4,11 +4,12 @@ pragma abicoder v2;
 
 import "../abstract/AbstractLeveragedPool.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./PoolToken.sol";
 
 /*
 @title The pool controller contract
 */
-contract LeveragedPool is AccessControl {
+contract LeveragedPool is AbstractLeveragedPool, AccessControl {
   // #### Roles
   /**
   @notice The Updater role is for addresses that can update a pool's price
@@ -21,6 +22,46 @@ contract LeveragedPool is AccessControl {
   bytes32 public constant FEE_HOLDER = keccak256("FEE_HOLDER");
 
   // #### Functions
+  /**
+  @notice Sets up the contract. Sets the parameters, and creates the two ERC20 tokens for the pool.
+  @param _poolCode The pool's identifier. This will be appended onto a position code (-SHORT, and -LONG) to create the token names.
+  @param _firstPrice The current price for the market
+  @param _updateInterval The minimum amount of time that must elapse before a price update can occur. If the interval is 5 minutes, then the price cannot be updated until 5 minutes after the last update has elapsed.
+  @param _frontRunningInterval The amount of time that must elapse between a commit and the next update interval before a commit can be executed. Must be shorter than the update interval to prevent deadlock.
+  @param _fee The percentage fee that will be charged to the pool's capital on a successful price update
+  @param _leverageAmount The leverage that the pool will expose it's depositors to
+  @param _feeAddress The address that fees will be sent to on every price change
+  @param _quoteToken The address of the digital asset that this pool contains
+   */
+  constructor(
+    string memory _poolCode,
+    uint256 _firstPrice,
+    uint32 _updateInterval,
+    uint32 _frontRunningInterval,
+    uint16 _fee,
+    uint16 _leverageAmount,
+    address _feeAddress,
+    address _quoteToken
+  ) {
+    quoteToken = _quoteToken;
+    lastPrice = _firstPrice;
+    updateInterval = _updateInterval;
+    frontRunningInterval = _frontRunningInterval;
+    fee = _fee;
+    leverageAmount = _leverageAmount;
+    feeAddress = _feeAddress;
+
+    tokens[0] = new PoolToken(
+      abi.encodePacked(_poolCode, "-LONG"),
+      abi.encodePacked("L-", _poolCode)
+    );
+    tokens[1] = new PoolToken(
+      abi.encodePacked(_poolCode, "-SHORT"),
+      abi.encodePacked("S-", _poolCode)
+    );
+
+    emit TokensCreated(tokens[0], tokens[1], _firstPrice, _quoteToken);
+  }
 
   // #### Modifiers
   /**
