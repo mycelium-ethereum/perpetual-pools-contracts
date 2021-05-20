91b806e3af9da4eaaa44e0084f635f9287114e2d
Justin Taylor <justin@labrys.io>
2021-05-21 09:21:07 +1000

    added access control to oracle

--- contracts/implementation/OracleWrapper.sol
+++ contracts/implementation/OracleWrapper.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "../interfaces/IOracleWrapper.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /*
 @title The oracle management contract
 */
-contract OracleWrapper is IOracleWrapper {
+contract OracleWrapper is IOracleWrapper, AccessControl {
   // #### Globals
   /**
   @notice Format: Market code => oracle address. Market code looks like TSLA/USD+aDAI
@@ -19,13 +20,24 @@ contract OracleWrapper is IOracleWrapper {
   bytes32 public constant OPERATOR = keccak256("OPERATOR");
 
   // #### Functions
+  constructor() {
+    grantRole(OPERATOR, msg.sender);
+  }
+
   function setOracle(string memory marketCode, address oracle)
     external
     override
+    onlyOperator
   {}
 
   function getPrice(string memory marketCode, address oracle)
     external
     override
   {}
+
+  // #### Modifiers
+  modifier onlyOperator {
+    require(hasRole(OPERATOR, msg.sender));
+    _;
+  }
 }
