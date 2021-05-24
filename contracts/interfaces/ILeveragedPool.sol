2090f08dc1c40524f9dbb86ffe852c4b6805b5c7
Justin Taylor <justin@labrys.io>
2021-05-24 10:50:28 +1000

    tests passing

--- contracts/interfaces/ILeveragedPool.sol
+++ contracts/interfaces/ILeveragedPool.sol
@@ -7,12 +7,7 @@ pragma abicoder v2;
 */
 interface ILeveragedPool {
   // #### Struct & Enum definitions
-  enum CommitType {
-    ShortMint;
-    ShortBurn;
-    LongMint;
-    LongBurn;  
-    }
+  enum CommitType { ShortMint, ShortBurn, LongMint, LongBurn }
   struct Commit {
     uint256 created;
     uint256 amount;
