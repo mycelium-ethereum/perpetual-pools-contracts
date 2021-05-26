8f6159607021d21e1a0edfbe6ff76e8e33f99cf2
Justin Taylor <justin@labrys.io>
2021-05-26 17:14:08 +1000

    defined test cases

---
+++ test/LeveragedPool/uncommit.spec.ts
@@ -0,0 +1,8 @@
+describe("LeveragedPool - uncommit", () => {
+  it("should allow the owner of a commit delete that commit", async () => {});
+  it("should remove a commit from storage", async () => {});
+  it("should emit an event for uncommitting", async () => {});
+  it("should refund the user's committed tokens", async () => {});
+  it("should revert if the commit doesn't exist", async () => {});
+  it("should revert if an account other than the owner tries to uncommit a commitment", async () => {});
+});
