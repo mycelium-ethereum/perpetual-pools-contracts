b856b2eee1b4f0d271ca7b018e22fd9fb88e4de3
Justin Taylor <justin@labrys.io>
2021-06-24 14:11:17 +1000

    tests for updateFeeAddress passing

--- test/LeveragedPool/updateFeeAddress.spec.ts
+++ test/LeveragedPool/updateFeeAddress.spec.ts
@@ -1,12 +1,19 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
+import chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { ethers } from "hardhat";
 import { LeveragedPool } from "../../typechain";
 import { POOL_CODE } from "../constants";
+import { deployPoolAndTokenContracts } from "../utilities";
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
 
 describe("LeveragedPool - updateFeeAddress", () => {
   let pool: LeveragedPool;
   let signers: SignerWithAddress[];
   beforeEach(async () => {
+    signers = await ethers.getSigners();
     const result = await deployPoolAndTokenContracts(
       POOL_CODE,
       5,
@@ -15,18 +22,17 @@ describe("LeveragedPool - updateFeeAddress", () => {
       signers[0].address,
       500
     );
-    signers = result.signers;
     pool = result.pool;
   });
   it("should set fee address", async () => {
-    expect(await pool.feeAddress).to.eq(signers[0].address);
+    expect(await pool.feeAddress()).to.eq(signers[0].address);
     await pool.updateFeeAddress(signers[1].address);
-    expect(await pool.feeAddress).to.eq(signers[1].address);
+    expect(await pool.feeAddress()).to.eq(signers[1].address);
   });
   it("should prevent unauthorized access", async () => {
     await pool.updateFeeAddress(signers[1].address);
-    await expect(pool.updateFeeAddress(signers[2].address)).to.be.rejectedWith(
-      Error
-    );
+    await expect(
+      pool.connect(signers[2]).updateFeeAddress(signers[2].address)
+    ).to.be.rejectedWith(Error);
   });
 });
