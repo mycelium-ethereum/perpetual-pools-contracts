fa75ce65e99d8c003d801718aedcd492ff3afd5a
Justin Taylor <justin@labrys.io>
2021-06-30 11:04:00 +1000

    draft deploy script done

---
+++ scripts/contracts.ts
@@ -0,0 +1,91 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ethers } from "hardhat";
+import {
+  PoolKeeper__factory,
+  OracleWrapper,
+  PoolSwapLibrary__factory,
+  PoolFactory__factory,
+  OracleWrapper__factory,
+} from "../typechain";
+import { abi as OracleWrapperABI } from "../artifacts/contracts/implementation/OracleWrapper.sol/OracleWrapper.json";
+
+let deployer: SignerWithAddress;
+
+export const deployPoolSwapLibrary = async (): Promise<string> => {
+  if (!deployer) {
+    deployer = (await ethers.getSigners())[0];
+  }
+  const factory = (await ethers.getContractFactory(
+    "PoolSwapLibrary",
+    deployer
+  )) as PoolSwapLibrary__factory;
+  return (await (await factory.deploy()).deployed()).address;
+};
+
+/**
+ * Deploys an instance of the PoolFactory contract
+ * @returns string  The address of the newly deployed factory
+ */
+export const deployPoolFactory = async (
+  libraryAddress: string
+): Promise<string> => {
+  if (!deployer) {
+    deployer = (await ethers.getSigners())[0];
+  }
+  const factory = (await ethers.getContractFactory("PoolFactory", {
+    signer: deployer,
+    libraries: { PoolSwapLibrary: libraryAddress },
+  })) as PoolFactory__factory;
+
+  return (await (await factory.deploy()).deployed()).address;
+};
+
+/**
+ * Deploys an instance of the OracleWrapper contract
+ * @returns string  The address of the new oracle wrapper
+ */
+export const deployOracleWrapper = async (): Promise<string> => {
+  if (!deployer) {
+    deployer = (await ethers.getSigners())[0];
+  }
+  const factory = (await ethers.getContractFactory("OracleWrapper", {
+    signer: deployer,
+  })) as OracleWrapper__factory;
+
+  return (await (await factory.deploy()).deployed()).address;
+};
+
+/**
+ * Deploys an instance of the PoolKeeper contract and grants the operator role to it in the oracle wrapper
+ * @param oracleWrapperAddress  The address of the oracle wrapper to use when creating markets
+ * @param factoryAddress  The address of the factory to use when creating pools
+ * @returns string  The address of the pool keeper instance
+ */
+export const deployPoolKeeper = async (
+  oracleWrapperAddress: string,
+  factoryAddress: string
+): Promise<string> => {
+  if (!deployer) {
+    deployer = (await ethers.getSigners())[0];
+  }
+  const factory = (await ethers.getContractFactory("PoolKeeper", {
+    signer: deployer,
+  })) as PoolKeeper__factory;
+
+  const poolKeeperAddress = (
+    await (
+      await factory.deploy(oracleWrapperAddress, factoryAddress)
+    ).deployed()
+  ).address;
+
+  // Grant permission for the new keeper to add markets to the oracle wrapper
+  const oracle = new ethers.Contract(
+    oracleWrapperAddress,
+    OracleWrapperABI,
+    deployer
+  ) as OracleWrapper;
+  const operatorRole = await oracle.OPERATOR();
+  await oracle.grantRole(operatorRole, poolKeeperAddress);
+
+  return poolKeeperAddress;
+};
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,138 @@
-import { ethers } from "hardhat";
+#!/usr/bin/env node
+import {
+  deployPoolKeeper,
+  deployOracleWrapper,
+  deployPoolFactory,
+  deployPoolSwapLibrary,
+} from "./contracts";
+import { parse } from "ts-command-line-args";
 
-async function main() {
-  const factory = await ethers.getContractFactory("Counter");
-
-  // If we had constructor arguments, they would be passed into deploy()
-  let contract = await factory.deploy();
-
-  // The address the Contract WILL have once mined
-  console.log(contract.address);
-
-  // The transaction that was sent to the network to deploy the Contract
-  console.log(contract.deployTransaction.hash);
-
-  // The contract is NOT deployed yet; we must wait until it is mined
-  await contract.deployed();
+interface IDeploymentArgs {
+  contracts?: string[];
+  all?: boolean;
+  factory?: string;
+  oracle?: string;
+  library?: string;
 }
+/**
+ * Deploys one or more of the project's contracts.
+ * Available options:
+ * -- contracts with one or more contract names (eg --contracts PoolFactory PoolKeeper). If you are not deploying the oracle, factory, and/or library, you must provide the addresses
+ * --factory The address of a deployed factory
+ * --oracle The address of a deployed oracle wrapper
+ * --library The address of a deployed pool swap library
+ * --all Deploy everything fresh. Ignores all other flags.
+ */
+const deploy = async (): Promise<void> => {
+  const argv = parse<IDeploymentArgs>({
+    contracts: { type: String, multiple: true, optional: true },
+    all: { type: Boolean, multiple: false, optional: true },
+    factory: { type: String, multiple: false, optional: true },
+    oracle: { type: String, multiple: false, optional: true },
+    library: { type: String, multiple: false, optional: true },
+  });
 
-main()
+  const getIsDeploying = (name: string) =>
+    argv.contracts?.some(
+      (el: string) => el?.toLowerCase() === name.toLowerCase()
+    );
+
+  const isDeployingContract = {
+    oracleWrapper: getIsDeploying("OracleWrapper"),
+    poolKeeper: getIsDeploying("PoolKeeper"),
+    poolFactory: getIsDeploying("PoolFactory"),
+    poolLibrary: getIsDeploying("PoolSwapLibrary"),
+  };
+
+  let oracle: string | undefined = undefined;
+  let factory: string | undefined = undefined;
+  let library: string | undefined = undefined;
+
+  if (argv.all) {
+    console.log(`
+    Deploying all contracts\n 
+    Now deploying PoolSwapLibrary...\n
+    `);
+    library = await deployPoolSwapLibrary();
+    console.log("Library address is: ", library);
+
+    console.log("Now deploying PoolFactory...\n");
+    factory = await deployPoolFactory(library);
+    console.log("PoolFactory address is ", factory);
+
+    console.log("Now deploying OracleWrapper...\n");
+    oracle = await deployOracleWrapper();
+    console.log("OracleWrapper address is ", oracle);
+
+    console.log("Now deploying PoolKeeper...\n");
+    console.log(
+      "PoolKeeper address is ",
+      await deployPoolKeeper(oracle, factory)
+    );
+    console.log("Deployed all contracts, exiting");
+    return;
+  }
+
+  if (isDeployingContract.oracleWrapper) {
+    console.log("Now deploying OracleWrapper...\n");
+    oracle = await deployOracleWrapper();
+    console.log("OracleWrapper address is ", oracle);
+  }
+  if (isDeployingContract.poolLibrary) {
+    console.log("Now deploying PoolSwapLibrary...\n");
+    library = await deployPoolSwapLibrary();
+    console.log("Library address is: ", library);
+  }
+  if (isDeployingContract.poolFactory) {
+    if (!library && !argv.library) {
+      throw new Error("Library deployed or address not provided");
+    }
+    console.log("Now deploying PoolFactory...\n");
+    if (library) {
+      factory = await deployPoolFactory(library);
+    } else if (argv?.library) {
+      factory = await deployPoolFactory(argv.library);
+    }
+    console.log("PoolFactory address is ", factory);
+  }
+  if (isDeployingContract.poolKeeper) {
+    if (!oracle && !argv?.oracle) {
+      throw new Error("OracleWrapper not deployed or address not provided");
+    }
+    if (!factory && !argv.factory) {
+      throw new Error("Factory not deployed or address not provided");
+    }
+    console.log("Now deploying PoolKeeper...\n");
+    if (oracle) {
+      if (factory) {
+        console.log(
+          "PoolKeeper address is ",
+          await deployPoolKeeper(oracle, factory)
+        );
+      } else if (argv?.factory) {
+        console.log(
+          "PoolKeeper address is ",
+          await deployPoolKeeper(oracle, argv?.factory)
+        );
+      }
+    } else if (argv?.oracle) {
+      if (factory) {
+        console.log(
+          "PoolKeeper address is ",
+          await deployPoolKeeper(argv?.oracle, factory)
+        );
+      } else if (argv?.factory) {
+        console.log(
+          "PoolKeeper address is ",
+          await deployPoolKeeper(argv?.oracle, argv?.factory)
+        );
+      }
+    }
+  }
+  console.log(`Deployment complete, exiting...`);
+};
+
+deploy()
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
