ccdcb47f4609577142620db5f1deac1348fe2e20
BenjaminPatch <benpatch@hotmail.com>
2021-08-06 11:44:57 +1000

    lint

--- contracts/implementation/LeveragedPool.sol
+++ contracts/implementation/LeveragedPool.sol
@@ -249,11 +249,11 @@ contract LeveragedPool is ILeveragedPool, AccessControl, Initializable {
     }
 
     /**
-     * @notice Processes the effect of a price change. The effect of a price change on a pool is left to the implementer. The pool stores the last price, and is given the latest price on update. 
+     * @notice Processes the effect of a price change. The effect of a price change on a pool is left to the implementer. The pool stores the last price, and is given the latest price on update.
      * @dev This function should be called by the Pool Keeper.
      * @dev This function should be secured with some form of access control
      * @param oldPrice The previously executed price
-     * @param newPrice The price for the latest interval. 
+     * @param newPrice The price for the latest interval.
      */
     function executePriceChange(int256 oldPrice, int256 newPrice) external override onlyUpdater {
         require(intervalPassed(), "Update interval hasn't passed");
