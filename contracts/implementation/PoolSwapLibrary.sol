465b6ffdbdb816c4b4f24d18f22616658b5bdc4b
Madhav Kumar <47245900+kumar-ish@users.noreply.github.com>
2021-08-12 01:09:21 +1000

    Remove stray abicoder references in contracts

--- contracts/implementation/PoolSwapLibrary.sol
+++ contracts/implementation/PoolSwapLibrary.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
-pragma abicoder v2;
+
 import "abdk-libraries-solidity/ABDKMathQuad.sol";
 
 library PoolSwapLibrary {
--- contracts/test-utilities/TestPoolFactory.sol
+++ contracts/test-utilities/TestPoolFactory.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
-pragma abicoder v2;
 
 import "../implementation/LeveragedPool.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
