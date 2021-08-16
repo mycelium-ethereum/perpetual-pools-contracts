ac6c6169d025007d6624daf58826b821736b8b38
raymogg <raymondmogg@gmail.com>
2021-08-16 10:46:50 +1000

    lint

--- contracts/interfaces/IPoolToken.sol
+++ contracts/interfaces/IPoolToken.sol
@@ -2,16 +2,16 @@ pragma solidity 0.8.6;
 
 interface IPoolToken {
     /**
-    * @notice Mints pool tokens
-    * @param amount of pool tokens to mint
-    * @param account that the pool tokens are being minted to
-    */
+     * @notice Mints pool tokens
+     * @param amount of pool tokens to mint
+     * @param account that the pool tokens are being minted to
+     */
     function mint(uint256 amount, address account) external returns (bool);
 
     /**
-    * @notice Burns pool tokens
-    * @param amount of pool tokens to burn
-    * @param account that the pool tokens are being burned from
-    */
+     * @notice Burns pool tokens
+     * @param amount of pool tokens to burn
+     * @param account that the pool tokens are being burned from
+     */
     function burn(uint256 amount, address account) external returns (bool);
 }
