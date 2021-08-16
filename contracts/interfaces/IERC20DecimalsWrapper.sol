d33a297034f2717244a8590441dcda39a4c97f83
BenjaminPatch <benpatch@hotmail.com>
2021-08-16 10:09:00 +1000

    Add decimals wrapper

---
+++ contracts/interfaces/IERC20DecimalsWrapper.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+/**
+@title The oracle management contract interface
+*/
+interface IERC20DecimalsWrapper {
+    function decimals() external view returns (uint256);
+}
