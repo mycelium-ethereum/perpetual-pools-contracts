c8d8cfaccc71c90330593db1cd4dc97685e5491b
sporejack <jack.mcpherson@mycelium.ventures>
2021-09-02 15:53:48 +1000

    Bump solc 0.8.6 -> 0.8.7

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -24,7 +24,7 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     solidity: {
-        compilers: [{ version: "0.8.6", settings: {} }],
+        compilers: [{ version: "0.8.7", settings: {} }],
     },
     networks: {
         goerli: {
