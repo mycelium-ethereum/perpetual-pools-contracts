08055cc41b77852f1de0a02aa1daeec014b668b8
sporejack <jack.mcpherson@mycelium.ventures>
2021-09-09 13:42:59 +1000

    Add tests

--- test/PoolKeeper/setGasPrice.spec.ts
+++ test/PoolKeeper/setGasPrice.spec.ts
@@ -21,11 +21,14 @@ describe("PoolKeeper.setGasPrice", () => {
     let poolKeeper: PoolKeeper
     let factory: PoolFactory
     let signers: SignerWithAddress[]
+    let owner: SignerWithAddress
+    let nonOwner: SignerWithAddress
 
     beforeEach(async () => {
         // Deploy the contracts
         signers = await ethers.getSigners()
-        signers = await ethers.getSigners()
+        owner = signers[0]
+        nonOwner = signers[1]
         const setup = await deployPoolSetupContracts()
         const token = setup.token
         const oracleWrapper = setup.oracleWrapper
@@ -49,12 +52,22 @@ describe("PoolKeeper.setGasPrice", () => {
         }
     })
 
-    context("When called", async () => {
+    context("When called by the owner", async () => {
         it("Sets the gas price to the input", async () => {
             const newGasPrice: BigNumberish = 500
-            await poolKeeper.setGasPrice(newGasPrice)
+            await poolKeeper.connect(owner).setGasPrice(newGasPrice)
 
             expect(await poolKeeper.gasPrice()).to.be.eq(newGasPrice)
+        })
+    })
+
+    context("When called by a non-owner", async () => {
+        it("Reverts", async () => {
+            const newGasPrice: BigNumberish = 500
+
+            await expect(
+                poolKeeper.connect(nonOwner).setGasPrice(newGasPrice)
+            ).to.be.revertedWith("")
         })
     })
 })
