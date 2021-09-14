2c57d03a67dde2cd459d5d43c2fc38cd49865b83
sporejack <jack.mcpherson@mycelium.ventures>
2021-09-14 13:36:59 +1000

    Remove fixed target

--- contracts/implementation/Administrator.sol
+++ contracts/implementation/Administrator.sol
@@ -9,20 +9,14 @@ import "../interfaces/IAdministrator.sol";
 import "../implementation/LeveragedPool.sol";
 
 contract Administrator is Ownable, IAdministrator {
-    address public target;
     IInbox public inbox;
 
-    constructor(address _target) {
-        require(_target != address(0), "Target address cannot be null");
-        target = _target;
+    constructor(address _inbox) {
+        require(_inbox != address(0), "ADMIN: Inbox address cannot be null");
+        inbox = IInbox(_inbox);
     }
 
-    function setTarget(address _target) external onlyOwner {
-        require(_target != address(0), "Target address cannot be null");
-        target = _target;
-    }
-
-    function callL2(bytes memory payload) public payable onlyOwner returns (uint256) {
+    function callL2(address target, bytes memory payload) public payable onlyOwner returns (uint256) {
         uint256 max_submission_cost = 1;
         uint256 max_gas = 1;
         uint256 gas_price_bid = 1;
