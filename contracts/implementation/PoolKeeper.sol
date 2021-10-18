0b7e1130d820ed7b7ce9fe2f62dafe55112d3fe0
sporejack <jack.mcpherson@mycelium.ventures>
2021-10-18 13:00:15 +1000

    Fixes code-423n4/2021-10-tracer-findings#5

--- contracts/implementation/PoolKeeper.sol
+++ contracts/implementation/PoolKeeper.sol
@@ -22,6 +22,7 @@ contract PoolKeeper is IPoolKeeper, Ownable {
     uint256 public constant TIP_DELTA_PER_BLOCK = 5; // 5% increase per block
     uint256 public constant BLOCK_TIME = 13; /* in seconds */
     uint256 public constant MAX_DECIMALS = 18;
+    uint256 public constant MAX_TIP = 100; /* maximum keeper tip */
 
     // #### Global variables
     /**
@@ -226,8 +227,8 @@ contract PoolKeeper is IPoolKeeper, Ownable {
         uint256 keeperTip = BASE_TIP + (TIP_DELTA_PER_BLOCK * elapsedBlocksNumerator) / BLOCK_TIME;
 
         // In case of network outages or otherwise, we want to cap the tip so that the keeper cost isn't unbounded
-        if (keeperTip > 100) {
-            return 100;
+        if (keeperTip > MAX_TIP) {
+            return MAX_TIP;
         } else {
             return keeperTip;
         }
