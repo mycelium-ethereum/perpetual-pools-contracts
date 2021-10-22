f47281fd2e6de91c736f150eb325f7e54c1ffd82
sporejack <jack.mcpherson@mycelium.ventures>
2021-10-22 15:47:26 +1000

    Add missing deployments for dependent contracts

--- test/SMAOracle.spec.ts
+++ test/SMAOracle.spec.ts
@@ -2,6 +2,12 @@ import { ethers } from "hardhat"
 import chai from "chai"
 import chaiAsPromised from "chai-as-promised"
 import {
+    PoolSwapLibrary,
+    PoolSwapLibrary__factory,
+    PoolFactory,
+    PoolFactory__factory,
+    PoolKeeper,
+    PoolKeeper__factory,
     ChainlinkOracleWrapper__factory,
     ChainlinkOracleWrapper,
     TestChainlinkOracle__factory,
@@ -22,9 +28,11 @@ describe("SMAOracle", async () => {
     let spotOracle: ChainlinkOracleWrapper
     let chainlinkOracle: TestChainlinkOracle
     let priceObserver: PriceObserver
+    let poolKeeper: PoolKeeper
     let signers: SignerWithAddress[]
     let owner: SignerWithAddress
     let nonOwner: SignerWithAddress
+    let feeReceiver: SignerWithAddress
     let numPeriods: BigNumberish
 
     before(async () => {
@@ -32,17 +40,50 @@ describe("SMAOracle", async () => {
         signers = await ethers.getSigners()
         owner = signers[0]
         nonOwner = signers[1]
+        feeReceiver = signers[2]
 
         /* configure deployment parameters */
         numPeriods = 5
 
+        /* deploy PoolSwapLibrary (PoolFactory needs to be linked to it) */
+        const poolSwapLibraryFactory: PoolSwapLibrary__factory =
+            (await ethers.getContractFactory(
+                "PoolSwapLibrary",
+                signers[0]
+            )) as PoolSwapLibrary__factory
+        const poolSwapLibrary: PoolSwapLibrary =
+            await poolSwapLibraryFactory.deploy()
+        await poolSwapLibrary.deployed()
+
+        /* deploy PoolFactory (PoolKeeper needs it) */
+        const poolFactoryFactory = (await ethers.getContractFactory(
+            "PoolFactory",
+            {
+                signer: signers[0],
+                libraries: { PoolSwapLibrary: poolSwapLibrary.address },
+            }
+        )) as PoolFactory__factory
+        const poolFactory = await poolFactoryFactory.deploy(feeReceiver.address)
+        await poolFactory.deployed()
+
+        /* deploy PoolKeeper */
+        const poolKeeperFactory = (await ethers.getContractFactory(
+            "PoolKeeper",
+            {
+                signer: signers[0],
+                libraries: { PoolSwapLibrary: poolSwapLibrary.address },
+            }
+        )) as PoolKeeper__factory
+        poolKeeper = await poolKeeperFactory.deploy(feeReceiver.address)
+        await poolKeeper.deployed()
+
         /* deploy test Chainlink oracle (we need something to feed into the wrapper) */
-        signers = await ethers.getSigners()
         const chainlinkOracleFactory = (await ethers.getContractFactory(
             "TestChainlinkOracle",
             signers[0]
         )) as TestChainlinkOracle__factory
         chainlinkOracle = await chainlinkOracleFactory.deploy()
+        await chainlinkOracle.deployed()
 
         /* deploy spot oracle contract */
         const spotOracleFactory = (await ethers.getContractFactory(
@@ -50,6 +91,7 @@ describe("SMAOracle", async () => {
             owner
         )) as ChainlinkOracleWrapper__factory
         spotOracle = await spotOracleFactory.deploy(chainlinkOracle.address)
+        await spotOracle.deployed()
 
         /* deploy price observer contract */
         const priceObserverFactory = (await ethers.getContractFactory(
@@ -57,6 +99,7 @@ describe("SMAOracle", async () => {
             owner
         )) as PriceObserver__factory
         priceObserver = await priceObserverFactory.deploy()
+        await priceObserver.deployed()
 
         /* deploy SMA oracle contract */
         const smaOracleFactory = (await ethers.getContractFactory(
@@ -69,6 +112,7 @@ describe("SMAOracle", async () => {
             priceObserver.address,
             numPeriods
         )
+        await smaOracle.deployed()
     })
 
     describe("SMA", async () => {
