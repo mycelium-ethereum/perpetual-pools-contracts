32dc37993ae2479440b990198ad50787b7d91723
0xhrithik <hrithik@mycelium.ventures>
2022-01-14 17:56:27 +0530

    add deployer address check

--- contracts/implementation/ChainlinkOracleWrapper.sol
+++ contracts/implementation/ChainlinkOracleWrapper.sol
@@ -18,6 +18,7 @@ contract ChainlinkOracleWrapper is IOracleWrapper {
     // #### Functions
     constructor(address _oracle, address _deployer) {
         require(_oracle != address(0), "Oracle cannot be 0 address");
+        require(_deployer != address(0), "Deployer cannot be 0 address");
         oracle = _oracle;
         deployer = _deployer;
         // reset the scaler for consistency
