61d119e0410fdf9c7da4c22144bc5fdc94e218cf
my_name_uhh <my_name_uhh@protonmail.com>
2022-03-07 14:50:50 +1000

    Apply suggestions from code review
    Co-authored-by: sporejack <72908791+sporejack@users.noreply.github.com>

--- test/PoolCommitter/executeCommitment/tokenPrice.spec.ts
+++ test/PoolCommitter/executeCommitment/tokenPrice.spec.ts
@@ -42,7 +42,7 @@ describe("PoolCommitter - executeCommitment: Token Price", () => {
     let poolCommitter: PoolCommitter
     let pool: LeveragedPool
     let signers: SignerWithAddress[]
-    describe("Short Burn and mint", () => {
+    describe("Short burn and mint", () => {
         it("Should account for burns allocated to future update intervals", async () => {
             const result = await deployPoolAndTokenContracts(
                 POOL_CODE,
@@ -62,7 +62,6 @@ describe("PoolCommitter - executeCommitment: Token Price", () => {
             await timeout(updateInterval * 1000)
             await pool.poolUpkeep(10, 10)
             // Before FR interval
-            console.log(1)
             await createCommit(
                 poolCommitter,
                 [SHORT_BURN],
@@ -80,18 +79,16 @@ describe("PoolCommitter - executeCommitment: Token Price", () => {
             )
 
             // Now first commit can be executed
-            console.log((await token.balanceOf(signers[0].address)).toString())
             const balBefore = await token.balanceOf(signers[0].address)
             await timeout(frontRunningInterval * 1000)
             await pool.poolUpkeep(10, 10)
             await poolCommitter.claim(signers[0].address)
-            console.log((await token.balanceOf(signers[0].address)).toString())
             const balAfter = await token.balanceOf(signers[0].address)
             expect(balAfter).to.equal(balBefore.add(amountCommitted.div(4)))
         })
     })
     describe("Long Burn and mint", () => {
-        it.only("Should account for burns allocated to future update intervals", async () => {
+        it("Should account for burns allocated to future update intervals", async () => {
             const result = await deployPoolAndTokenContracts(
                 POOL_CODE,
                 frontRunningInterval,
@@ -110,7 +107,6 @@ describe("PoolCommitter - executeCommitment: Token Price", () => {
             await timeout(updateInterval * 1000)
             await pool.poolUpkeep(10, 10)
             // Before FR interval
-            console.log(1)
             await createCommit(
                 poolCommitter,
                 [LONG_BURN],
@@ -128,12 +124,10 @@ describe("PoolCommitter - executeCommitment: Token Price", () => {
             )
 
             // Now first commit can be executed
-            console.log((await token.balanceOf(signers[0].address)).toString())
             const balBefore = await token.balanceOf(signers[0].address)
             await timeout(frontRunningInterval * 1000)
             await pool.poolUpkeep(10, 10)
             await poolCommitter.claim(signers[0].address)
-            console.log((await token.balanceOf(signers[0].address)).toString())
             const balAfter = await token.balanceOf(signers[0].address)
             expect(balAfter).to.equal(balBefore.add(amountCommitted.div(4)))
         })
