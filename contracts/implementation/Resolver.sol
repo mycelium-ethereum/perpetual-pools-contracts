12f4ec7db8c87f1817db76b052cc4434b35bd1af
0xhrithik <hrithik@mycelium.ventures>
2022-03-28 14:58:54 +0530

    add coms

--- contracts/implementation/Resolver.sol
+++ contracts/implementation/Resolver.sol
@@ -5,8 +5,8 @@ import "../interfaces/IResolver.sol";
 import "../interfaces/IPoolFactory.sol";
 import "../interfaces/IPoolKeeper.sol";
 
-/// @title The resolver contract is responsible for checking if a pool requires to be upkept
-/// @dev if upkeep is required for a pool then `upKeepChecker` returns the payload of the `isUpkeepRequiredSinglePool` function with the pool address as argument
+/// @title The resolver contract is responsible for periodically checking if a pool requires to be upkept
+/// @notice This contract will be used in gelato app to create a gelato task for upkeeping pools.
 contract Resolver is IResolver {
     IPoolFactory public poolFactory;
     IPoolKeeper public poolKeeper;
@@ -16,6 +16,10 @@ contract Resolver is IResolver {
         poolKeeper = IPoolKeeper(poolFactory.getPoolKeeper());
     }
 
+    /**
+     * @notice Iterate through all pools and will then return true if can be upkept, and data including the function selector and parameters.
+     * @return execPayLoad payload data of the `isUpkeepRequiredSinglePool` function selector with the pool address as argument
+     */
     function upKeepChecker() external view override returns (bytes[] memory execPayLoad) {
         uint256 poolsLength = poolFactory.numPools();
         execPayLoad = new bytes[](poolsLength);
