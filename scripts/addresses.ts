db4c20bfe499f49df0095eb0494dc1ef7fb39915
Dospore <tetthera@gmail.com>
2023-05-09 15:52:17 +0300

    Add deploy inflation pool

--- scripts/addresses.ts
+++ scripts/addresses.ts
@@ -22,6 +22,7 @@ export const arbitrumMainnet: NetworkAddresses = {
     devMultisig: "0x0f79e82aE88E1318B8cfC8b4A205fE2F982B928A",
     btcUsdChainlinkFeed: "0x6ce185860a4963106506C203335A2910413708e9",
     ethUsdChainlinkFeed: "0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612",
+    tfiUsdChainlinkFeeed: "0xF0ffC609da91d1931314BA5d17F1786db985D801",
 }
 
 export const arbitrumRinkeby: NetworkAddresses = {
@@ -35,4 +36,5 @@ export const arbitrumRinkeby: NetworkAddresses = {
     devMultisig: "0xb0Ad46bD50b44cBE47E2d83143E0E415d6A842F6", // Dummy value. Feel free to change this to whatever you want.
     btcUsdChainlinkFeed: "0x0c9973e7a27d00e656B9f153348dA46CaD70d03d",
     ethUsdChainlinkFeed: "0x5f0423B1a6935dc5596e7A24d98532b67A0AeFd8",
+    tfiUsdChainlinkFeeed: "0x9De602408AA53F0BB8bC54280A9fb70446289cFC",
 }
